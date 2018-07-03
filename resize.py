47be0743296ff6caf9bdaea7dc6888a9ffb04def
bill41708 <33356634+bill41708@users.noreply.github.com>
2018-07-03 17:00:04 +0800

    Add files via upload

--- resize.py
+++ resize.py
@@ -19,5 +19,30 @@ for filename in os.listdir(path):
         test = test.resize((180,240),Image.BILINEAR)
         test.save(path+'\\'+filename)
 
+"""
+def resize_image(image, height = IMAGE_SIZE, width = IMAGE_SIZE):
 
-
+    h, w, _ = image.shape
+    
+    #最長的一邊
+    longest_edge = max(h, w)    
+    
+    #哪邊要加
+    if h < longest_edge:
+        dh = longest_edge - h
+        top = dh // 2
+        bottom = dh - top
+    elif w < longest_edge:
+        dw = longest_edge - w
+        left = dw // 2
+        right = dw - left
+    else:
+        pass 
+    
+    #顏色RGB
+    BLACK = [0, 0, 0]
+    
+    constant = cv2.copyMakeBorder(image, top , bottom, left, right, cv2.BORDER_CONSTANT, value = BLACK)
+    
+    return cv2.resize(constant, (height, width))
+"""
