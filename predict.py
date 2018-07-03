30b08a9dcd908acd158f6966e27cbd0a88962dcc
bill41708 <33356634+bill41708@users.noreply.github.com>
2018-07-04 02:10:06 +0800

    Add files via upload

--- predict.py
+++ predict.py
@@ -14,7 +14,7 @@ import os
 import numpy as np
 import operator
 
-model = load_model('model.h5')
+model = load_model('model6.h5')
 model.summary()
 
 #path=r".\testData"
@@ -24,21 +24,23 @@ for filename in os.listdir(path):
     if(filename[0] == 's'):
         test = Image.open(path+'\\'+filename)
         Ans = int(filename[1]) * 10 + int(filename[2])
+        temp = int(filename[4]) * 10 + int(filename[5])
         matrix = np.array(test) / 255
         matrix = matrix[np.newaxis,...]
         result = model.predict(matrix)
-        
+        #print(result.shape)
+		
         result_dict = {}
         for i in range(len(result[0])):
             result_dict[i] = float(result[0][i])
         sorted_result_dict = sorted(result_dict.items(), key=operator.itemgetter(1), reverse=True)
 
-        print("\nPredict Answer:")
+        print("\nPredict Ans:")
         print(sorted_result_dict[0][0])
-        print("\ncurrect Answer:")
+        print("\ncurrect Ans:")
         print(Ans)
 
-        print('\nTop 5 candidate: ')
+        print('\ntop-5 candidate: ')
         count = 0
         for topf in sorted_result_dict:
             count += 1
@@ -48,7 +50,8 @@ for filename in os.listdir(path):
 
         
         if(sorted_result_dict[0][0]!=Ans):
-            #pause=input('\nCheck next one')
+            print(Ans,temp)
+            #pause=input('\nUU')
             error += 1
 
 print('error:' , error) 
